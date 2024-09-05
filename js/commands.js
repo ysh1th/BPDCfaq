b678084e47ef80cb655cc301afe83ac1b10b2f70
Yashwanth Karuparthi <yashwanth782003@gmail.com>
2024-09-05 18:31:59 +0400

    init

---
+++ js/commands.js
@@ -0,0 +1,101 @@
+var x = 'https://x.com/BITSPilaniDubai';
+var linkedin = 'https://www.linkedin.com/school/bitspilanidubai/posts/?feedView=all';
+var youtube = 'https://www.youtube.com/channel/UClKXaO8gB10OObAkoEkyNDQ';
+var instagram = 'https://www.instagram.com/bitspilanidubai/';
+
+var portfolio = '';
+var github1 = "https://github.com/HUSAM-07";
+var github2 = "https://github.com/ysh1th";
+var uninotes = 'https://uni-notes.netlify.app/';
+var mtcnotes = 'https://mtcbpdcdubai.github.io/Coursework-Resources-and-Links/';
+var unicoords = 'https://www.google.com/search?q=coordinates+of+bits+pilani+dubai+campus&oq=coordinates+of+bits+pilani+dubai+campus&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQLhhA0gEINzMwOWowajGoAgCwAgA&sourceid=chrome&ie=UTF-8';
+var webopac = 'http://webopac.bits-dubai.ac.ae/AutoLib/';
+
+
+social = [
+  '<br>',
+  'X                <a href="' + x + '" target="_blank">x.com/BPDC' + "</a>",
+  'LinkedIn         <a href="' + linkedin + '" target="_blank">linkedin.com/BPDC' + "</a>",
+  'Youtube          <a href="' + youtube + '" target="_blank">youtube.com/BPDC' + "</a>",
+  'Instagram        <a href="' + instagram + '" target="_blank">instagram.com/BPDC' + "</a>",
+  "<br>",
+  'github profiles of the creators:',
+  
+  'github1         <a href="' + github1 + '" target="_blank">github.com/HUSAM-07' + "</a>",
+  'github2         <a href="' + github2 + '" target="_blank">github.com/ysh1th' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">getlyf</span>         How to get life in this uni?',
+  '<span class="command">examprep</span>       How can I prepare for exams?',
+  '<span class="command">pwdbl</span>          Direction for boys lounge?',
+  '<br>',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You already know what this does',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">sudo</span>           Only if you\'re admin, pls',
+  '<span class="command">cd..</span>           Go back to portfolio',
+]
+
+banner = [
+  '<span class="index">Welcome to BPDC FAQ. All BITS reserved.</span>',
+  "        ,,                               .-. ",
+  "       || |                               ) ) ",
+  "       || |   ,                          '-'  ",
+  "       || |  | |                              ",
+  "       || '--' |                              ",
+  " ,,     || .----'                                ",
+  "|| |   || |       ",
+  "|  '---'| |       ",
+  "'------.| |                                  _____ ",
+  "       || |      (  _                       / /|\\ \\ ",
+  "       || |     ))(``),                    | | | | |  ",
+  "_______||_|_____((__^_))____________________\\_\\|/_/______________________________<span class=\"yellow\">__________________</span><span class=\'blue\'>__________________</span><span class=\'red\'>__________________</span>",
+  '<span class="color2">Welcome to my interactive web FAQ terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+getlyf = [
+  '<br>',
+  'Start by joining every club and activities that excite you!', 
+  'It’s the best way to meet people and make the most of your uni experience.', 
+  'And remember, <span class="darkyellow">a busy schedule is a happy schedule!</span>',
+  '<br>',
+];
+
+examprep = [
+  '<br>',
+  'You get to choose two pills, a <span class=\"red\">red pill</span> or a <span class=\"blue\">blue pill</span>.', 
+  'With <span class=\"red\">red pill</span> you take chill and will need late-night study sessions,',
+  'coffee and a good dose of panic-induced motivation.',
+  'With <span class=\"blue\">blue pill</span> you allocate some time everyday, or at least weekends for academics,',
+  'start working on assignments immediately after they are assigned,', 
+  'and use the campus resources available like: <br><a href="' + mtcnotes + '" target="_blank">MTC Notes</a>',
+  '<a href="' + uninotes + '" target="_blank">Uni Notes</a>',
+  '<a href="' + webopac + '" target="_blank">BPDC eLibrary</a>',
+  '<br>',
+];
+
+pwdbl = [
+  '<br>',
+  '     error <a href="' + unicoords + '" target="_blank">25N, 55E</a> not found ;(',
+  '<br>',
+  'Click to find about the error code',
+  '<br>',
+];
+
+
+
+
+
+
